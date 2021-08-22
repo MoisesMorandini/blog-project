0e6a000aaa24cd45e87347665083e30871739f6f
MoisesMorandini <moises-morandini@hotmail.com>
2021-08-22 20:14:23 -0300

    Handle click in Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,14 @@
 import commonStyle from '../../styles/common.module.scss';
 import styles from './header.module.scss'
+import Link from 'next/link';
 export default function Header() {
   return (
     <header className={`${commonStyle.applicationContainer} ${styles.headerContainer}`}>
-      <a href="">
-        <img src="/logo.svg" alt="logo" />
-      </a>
+      <Link href="/">
+        <a href="/">
+          <img src="/logo.svg" alt="logo" />
+        </a>
+      </Link>
     </header>
   )
 }
